656145d62291bc2205ed24b38e413702796a495d
LiChenhao <39847025+DesistDaydream@users.noreply.github.com>
2023-12-03 21:29:15 +0800

    增加元素控制示例

--- pkg/selenium/2.element_handling.py
+++ pkg/selenium/2.element_handling.py
@@ -2,27 +2,42 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 
-# 设置启动浏览器的参数
-options = webdriver.ChromeOptions()
-options.add_experimental_option("detach", True)  # 不要自动关闭浏览器
-driver = webdriver.Chrome(options=options)
+def initWebDriver() -> webdriver.Chrome:
+    # 设置启动浏览器的参数
+    options = webdriver.ChromeOptions()
+    options.add_experimental_option("detach", True)  # 不要自动关闭浏览器
+    driver = webdriver.Chrome(options=options)
+    driver.implicitly_wait(10)
 
-url = "https://passport.baidu.com/v2/?login"
-driver.get(url)
+    return driver
 
-# Selenium 的 WebElement 类是控制页面元素的核心。
-# 查找元素。返回 WebElement 实例。
-userlogButton = driver.find_element(By.XPATH, "//p[@title='用户名登录']")
-# 获取该元素中指定的属性的值
-print(userlogButton.get_attribute("class"))
-# 可以通过 outerHTML 关键字，获取整个元素，而不只获取特定的属性。这个用法在官方文档中没有任何示例？还是我没找到？
-print(userlogButton.get_attribute("outerHTML"))
-# 模拟点击该元素
-userlogButton.click()
 
-# 模拟输入
-# TODO: 在 input 标签中输入
+# 通过 XPath 定位元素
+def XPath(driver: webdriver.Chrome):
+    # 查找元素。返回 WebElement 实例。
+    btn = driver.find_element(By.XPATH, '//*[@id="my-check-2"]')
 
-# driver.find_element(By.XPATH, "//input[@placeholder='请输入手机号']").send_keys("")
-# driver.find_element(By.XPATH, "//input[@class='policy_tip-checkbox']").click()
-# driver.find_element(By.XPATH, "//button[text()='获取验证码']").click()
+    # 模拟点击该元素
+    btn.click()
+
+
+if __name__ == "__main__":
+    driver = initWebDriver()
+
+    url = "https://www.selenium.dev/selenium/web/web-form.html"
+    driver.get(url)
+
+    # Selenium 的 WebElement 类是控制页面元素的核心。
+    # find_element() 方法返回一个 WebElement 实例
+    ele = driver.find_element(By.CLASS_NAME, "form-control")
+
+    # WebElement 下有多个方法可以获取该元素的信息或处理该元素。
+    # 获取该元素中指定的属性的值
+    print(ele.get_attribute("class"))
+    # 可以通过 outerHTML 关键字，获取整个元素，而不只获取特定的属性。这个用法在官方文档中没有任何示例？还是我没找到？
+    print(ele.get_attribute("outerHTML"))
+    # 在该元素内输入内容（因为是 input 标签，如果是 button 标签则可以点击）
+    ele.send_keys("DesistDaydream")
+
+    # 通过 XPath 定位元素
+    XPath(driver)
